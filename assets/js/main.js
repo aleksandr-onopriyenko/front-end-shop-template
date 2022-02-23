591371393b8c9e593192a0e9d180c6cd6f385c0c
aleksandr-onopriyenko <new.area.for.study@gmail.com>
2022-02-23 15:10:45 +0200

    finish header block

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,63 +1,63 @@
-const contentContainer = document.querySelector('#content-container');
-const cartCounterLabel = document.querySelector('#cart-counter-label');
+// const contentContainer = document.querySelector('#content-container');
+// const cartCounterLabel = document.querySelector('#cart-counter-label');
 
-let cartCounter = 0;
-let cartPrice = 0;
+// let cartCounter = 0;
+// let cartPrice = 0;
 
-const incrementCounter = (label, cn) => {
-  const counter = cn + 1;
+// const incrementCounter = (label, cn) => {
+//   const counter = cn + 1;
 
-  label.innerHTML = `${counter}`;
-  if (counter === 1) cartCounterLabel.style.display = 'block';
+//   label.innerHTML = `${counter}`;
+//   if (counter === 1) cartCounterLabel.style.display = 'block';
 
-  return counter;
-};
+//   return counter;
+// };
 
-const getMockData = (t) => +t.parentElement
-  .previousElementSibling
-  .innerHTML
-  .replace(/^\$(\d+)\s\D+(\d+).*$/u, '$1.$2');
+// const getMockData = (t) => +t.parentElement
+//   .previousElementSibling
+//   .innerHTML
+//   .replace(/^\$(\d+)\s\D+(\d+).*$/u, '$1.$2');
 
-const getPrice = (t, price) => Math.round((price + getMockData(t)) * 100) / 100;
+// const getPrice = (t, price) => Math.round((price + getMockData(t)) * 100) / 100;
 
-const disableControls = (t, fn) => {
-  contentContainer.removeEventListener('click', fn);
-  t.disabled = true;
-};
+// const disableControls = (t, fn) => {
+//   contentContainer.removeEventListener('click', fn);
+//   t.disabled = true;
+// };
 
-const enableControls = (t, fn) => {
-  contentContainer.addEventListener('click', fn);
-  t.disabled = false;
-};
+// const enableControls = (t, fn) => {
+//   contentContainer.addEventListener('click', fn);
+//   t.disabled = false;
+// };
 
-const btnClickHandler = (e) => {
-  const target = e.target;
-  const interval = 2000;
+// const btnClickHandler = (e) => {
+//   const target = e.target;
+//   const interval = 2000;
 
-  let restoreHTML = null;
+//   let restoreHTML = null;
 
-  if (typeof target !== 'object') {
-    console.error('target не является объектом.');
+//   if (typeof target !== 'object') {
+//     console.error('target не является объектом.');
 
-    return;
-  }
+//     return;
+//   }
 
-  if (target.matches('.item-actions__cart')) {
+//   if (target.matches('.item-actions__cart')) {
 
-    cartCounter = incrementCounter(cartCounterLabel, cartCounter);
+//     cartCounter = incrementCounter(cartCounterLabel, cartCounter);
 
-    cartPrice = getPrice(target, cartPrice);
-    restoreHTML = target.innerHTML;
+//     cartPrice = getPrice(target, cartPrice);
+//     restoreHTML = target.innerHTML;
 
-    target.innerHTML = `Added ${cartPrice.toFixed(2)} $`;
+//     target.innerHTML = `Added ${cartPrice.toFixed(2)} $`;
 
-    disableControls(target, btnClickHandler);
+//     disableControls(target, btnClickHandler);
 
-    setTimeout(() => {
-      target.innerHTML = restoreHTML;
-      enableControls(target, btnClickHandler);
-    }, interval);
-  }
-};
+//     setTimeout(() => {
+//       target.innerHTML = restoreHTML;
+//       enableControls(target, btnClickHandler);
+//     }, interval);
+//   }
+// };
 
-contentContainer.addEventListener('click', btnClickHandler);
+// contentContainer.addEventListener('click', btnClickHandler);
